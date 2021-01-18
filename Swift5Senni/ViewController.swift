c156dd8d898253c698463dc9ef876325b671f310
vagrant <vagrant@localhost.localdomain>
2021-01-18 10:59:37 +0000

    first commit

---
+++ Swift5Senni/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift5Senni
+//
+//  Created by 長井崚介 on 2021/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
